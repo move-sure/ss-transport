df6714eb146cffa4c2ad8391ff1ba3a8a56f2916
Eklavya Singh <23f2001512@ds.study.iitm.ac.in>
2025-07-05 00:35:45 +0530

    updated

--- movesure-website/src/components/bilty/pdf-generation.js
+++ movesure-website/src/components/bilty/pdf-generation.js
@@ -1208,7 +1208,7 @@ const PDFGenerator = ({
             {!pdfUrl && !isGenerating && (
               <div className="bg-blue-50 border border-blue-200 rounded-lg p-4 mt-4 text-center">
                 <div className="text-blue-800 font-semibold mb-1">Ready to Generate</div>
-                <div className="text-blue-600 text-sm">Tap "Generate PDF" to create your document</div>
+                <div className="text-blue-600 text-sm">Tap &quot;Generate PDF&quot; to create your document</div>
               </div>
             )}
           </div>
